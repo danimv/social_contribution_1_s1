e4f337e5a31cc23b10a2f857b5a5dbfc084a4f85
danimv <danimv87@gmail.com>
2023-12-26 00:02:31 +0100

    Initial commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  secretKey: 'fjdi893jkde89nmkjd0xffkv'
+};
---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = require("path");
+
+// routes
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
+// app init
+const app = express();
+
+// body-parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// import mongoDB uri
+const database = "mongodb+srv://danimv87:dmv879731@foodup.xfpdje1.mongodb.net/contrib?retryWrites=true&w=majority";
+
+// connect to database
+mongoose
+  .connect(database)
+  .then(() => console.log("connected to MongoDB"))
+  .catch(err => console.log("db connection error"));
+
+// passport middleware
+app.use(passport.initialize());
+
+// passport config
+require("./config/passport")(passport);
+
+// routes
+app.use("/api/users", users);
+app.use("/api/posts", posts);
+app.use("/api/profile", profile);
+
+// serve static assets if in production mode
+
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server running on port ${port}`));
