93540e166477b80c948c5a90ab5635d3a4fdfbe0
danimv <danimv87@gmail.com>
2023-12-26 02:41:51 +0100

    a

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const passport = require("passport");
 const path = require("path");
 const cors = require('cors');
+require('dotenv').config();
 
 // routes
 const users = require("./routes/api/users");
@@ -29,7 +30,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // import mongoDB uri
-const database = "mongodb+srv://danimv87:dmv879731@foodup.xfpdje1.mongodb.net/contrib?retryWrites=true&w=majority";
+const database = process.env.MONGODB_URI;
 
 // connect to database
 mongoose
@@ -66,6 +67,6 @@ app.get('/', (req, res) => {
 //     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 //   });
 // }
-const port = 5000;//process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`server running on port ${port}`));
