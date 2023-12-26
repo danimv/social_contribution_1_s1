bcbe1878b58b5f03c44f6c07c77f12612f975ccc
danimv <danimv87@gmail.com>
2023-12-26 01:15:16 +0100

    a

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(cors({
   origin: true,
   credentials: true,  // Enable credentials (cookies, HTTP authentication) for cross-origin requests
 }));
+app.use(express.json({ limit: "10mb" }));
 
 // body-parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -48,14 +49,14 @@ app.get('/', (req, res) => {
 
 // serve static assets if in production mode
 
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("../client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   // Set static folder
+//   app.use(express.static("../client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`server running on port ${port}`));
