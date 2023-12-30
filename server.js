f7d04ebcc39570d9da21645b98e0a82c6a056c68
danimv <danimv87@gmail.com>
2023-12-30 18:53:29 +0100

    images server

--- server.js
+++ server.js
@@ -68,6 +68,12 @@ app.get('/', (req, res) => {
   res.send('Server is running');
 });
 
+app.get('/public/uploads/:filename', (req, res) => {
+  const filename = req.params.filename;
+  const imagePath = path.join(__dirname, 'public', 'uploads', filename);
+  res.sendFile(imagePath);
+});
+
 // serve static assets if in production mode
 
 // if (process.env.NODE_ENV === "production") {
@@ -78,6 +84,7 @@ app.get('/', (req, res) => {
 //     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 //   });
 // }
+// app.use('/public/uploads', express.static(path.join(__dirname, 'uploads')));
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`server running on port ${port}`));
